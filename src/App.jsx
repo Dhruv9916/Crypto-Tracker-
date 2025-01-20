53d61adbb220a949089edf5118621f82bcf3b302
Dhruv Kumar <113141712+Dhruv9916@users.noreply.github.com>
2025-01-20 23:18:13 +0530

    Integrating Mui Drawer in Landing Page

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css"
+import Header from './Components/Common/Header'
+import Footer from './Components/Common/Footer'
+
+const App = () => {
+  return (
+    <div>
+      <Header></Header>
+      {/* <Footer></Footer> */}
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Common/Header/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import "./style.css"
+import AnchorTemporaryDrawer from './drawer'
+
+function Header() {
+  return (
+    <div className="navbar">
+      <h1 className="logo">Crypto Tracker <span style={{color:"var(--blue)"}}>.</span></h1>
+      <div className="links">
+        <a href="/"><p className="link">Home</p></a>
+        <a href="/"><p className="link">Compare</p></a>
+        <a href="/"><p className="link">Watchlist</p></a>
+        <a href="/"><p className="link">Dashboard</p></a>
+        
+      </div>
+      <div  className="mobile-drawer">
+      <AnchorTemporaryDrawer></AnchorTemporaryDrawer>
+      </div>
+    </div>
+  )
+}
+
+export default Header
