d4b500f02a2efd68943e8223d66389e9368479f9
Dhruv Kumar <113141712+Dhruv9916@users.noreply.github.com>
2025-01-22 01:20:35 +0530

    Making Tabs Feature in Dashboard Page

--- src/Components/DashBoard/Tabs/index.jsx
+++ src/Components/DashBoard/Tabs/index.jsx
@@ -1,7 +1,42 @@
-import React from "react";
+import * as React from "react";
+import { useState } from "react";
 
-function TabsComponent() {
-  return <div>Tabs</div>;
+import Tab from "@mui/material/Tab";
+import TabContext from "@mui/lab/TabContext";
+import TabList from "@mui/lab/TabList";
+import TabPanel from "@mui/lab/TabPanel";
+import { createTheme, ThemeProvider } from "@mui/material";
+
+export default function TabsComponent() {
+  const [value, setValue] = useState("grid");
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  const style = {
+    color: "var(--white)",
+  };
+
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: "#3a80e9", // Custom primary color
+      },
+    },
+  });
+
+  return (
+    <ThemeProvider theme={theme}>
+      <TabContext value={value}>
+        <TabList onChange={handleChange} variant="fullWidth">
+          <Tab label="Grid" value="grid" sx={style} />
+          <Tab label="List" value="list" sx={style} />
+        </TabList>
+
+        <TabPanel value="grid">Grid</TabPanel>
+        <TabPanel value="list">List</TabPanel>
+      </TabContext>
+    </ThemeProvider>
+  );
 }
-
-export default TabsComponent;
