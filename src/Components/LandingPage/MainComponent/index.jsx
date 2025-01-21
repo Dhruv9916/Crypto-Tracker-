da803de34674f2335b0feb1063977caa5958e737
Dhruv Kumar <113141712+Dhruv9916@users.noreply.github.com>
2025-01-21 20:54:20 +0530

    Adding Framer Motion To Landing Page Component

--- src/Components/LandingPage/MainComponent/index.jsx
+++ src/Components/LandingPage/MainComponent/index.jsx
@@ -1,21 +1,66 @@
 import React from "react";
 import "./style.css";
 import Button from "../../Common/Button";
+import iphone from "../../../assets/iphone.png";
+import gradient from "../../../assets/gradient.png";
+import { motion } from "framer-motion";
 
 function MainComponent() {
-  return <div className="flex-info">
+  return (
+    <div className="flex-info">
       <div className="left-component">
-        <h1 className="track-crypto-heading">Track Crypto</h1>
-        <h1 className="real-time-heading">Real Time.</h1>
-        <p className="info-text">Track crypto with the public api in real time.Visit the Dashboard to do so!!</p>
-        <div className="btn-flex">
-          <Button text={"Dashboard"}/>
-          <Button text={"Share"} outlined={true}/>
-          
-        </div>
+        <motion.h1
+          className="track-crypto-heading"
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5 }}
+        >
+          Track Crypto
+        </motion.h1>
+        <motion.h1
+          className="real-time-heading"
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, delay: 0.5 }}
+        >
+          Real Time.
+        </motion.h1>
+        <motion.p
+          className="info-text"
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, delay: 0.75 }}
+        >
+          Track crypto with the public api in real time.Visit the Dashboard to
+          do so!!
+        </motion.p>
+        <motion.div
+          className="btn-flex"
+          initial={{ opacity: 0, x: -50 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.5, delay: 1.5 }}
+        >
+          <Button text={"Dashboard"} />
+          <Button text={"Share"} outlined={true} />
+        </motion.div>
       </div>
-      <div>Phone</div>
-  </div>;
+      <div className="phone-container">
+        <motion.img
+          src={iphone}
+          className="iphone"
+          animate={{
+            y: [0, -20, 0], // Moves up and down
+          }}
+          transition={{
+            duration: 3,
+            repeat: Infinity,
+            ease: "easeInOut",
+          }}
+        />
+        <img src={gradient} className="gradient" />
+      </div>
+    </div>
+  );
 }
 
 export default MainComponent;
