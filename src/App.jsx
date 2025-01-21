61bfdd6bfca7666942ff401782175885770649bf
Dhruv Kumar <113141712+Dhruv9916@users.noreply.github.com>
2025-01-21 15:49:10 +0530

    Landing Page left component is made

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css"
 import Header from './Components/Common/Header'
 import Footer from './Components/Common/Footer'
+import MainComponent from "./Components/LandingPage/MainComponent"
 
 const App = () => {
   return (
     <div>
       <Header></Header>
+      <MainComponent/>
       {/* <Footer></Footer> */}
     </div>
   )
---
+++ src/Components/LandingPage/MainComponent/index.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./style.css";
+import Button from "../../Common/Button";
+
+function MainComponent() {
+  return <div className="flex-info">
+      <div className="left-component">
+        <h1 className="track-crypto-heading">Track Crypto</h1>
+        <h1 className="real-time-heading">Real Time.</h1>
+        <p className="info-text">Track crypto with the public api in real time.Visit the Dashboard to do so!!</p>
+        <div className="btn-flex">
+          <Button text={"Dashboard"}/>
+          <Button text={"Share"} outlined={true}/>
+          
+        </div>
+      </div>
+      <div>Phone</div>
+  </div>;
+}
+
+export default MainComponent;
