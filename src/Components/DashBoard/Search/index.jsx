b449886760cc8f62643ad9cf14f36f14d8dd8776
Dhruv Kumar <113141712+Dhruv9916@users.noreply.github.com>
2025-01-23 20:24:07 +0530

    Search bar Functionality added

---
+++ src/Components/DashBoard/Search/index.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import "./style.css";
+import SearchIcon from "@mui/icons-material/Search";
+
+function Search({search,onSearchChange}) {
+  
+  return (
+    <div className="search-flex">
+      <SearchIcon />
+      <input
+        type="text"
+        placeholder="Search"
+        value={search}
+        onChange={(e) => onSearchChange(e)}
+      />
+    </div>
+  );
+}
+
+export default Search;
--- src/Pages/DashboardPage.jsx
+++ src/Pages/DashboardPage.jsx
@@ -2,9 +2,22 @@ import React, { useEffect, useState } from "react";
 import Header from "../Components/Common/Header";
 import TabsComponent from "../Components/DashBoard/Tabs";
 import axios from "axios";
+import Search from "../Components/DashBoard/Search";
 
 function DashboardPage() {
   const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState("");
+
+  const onSearchChange = (e) => {
+    console.log(e.target.value);
+    setSearch(e.target.value);
+  };
+
+  var filteredCoins = coins.filter(
+    (item) =>
+      item.name.toLowerCase().includes(search) ||
+      item.symbol.toLowerCase().includes(search)
+  );
 
   useEffect(() => {
     axios
@@ -23,7 +36,8 @@ function DashboardPage() {
   return (
     <div>
       <Header />
-      <TabsComponent coins={coins} />
+      <Search search={search} onSearchChange={onSearchChange}></Search>
+      <TabsComponent coins={filteredCoins} />
     </div>
   );
 }
